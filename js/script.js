f479fbc7760f5341dac4f848fd281dc10bdacc4a
Beast PJ <p.jadhav61657@gmail.com>
2024-09-05 00:05:53 +0530

    Screenshots/Gallery UI Improvement

--- js/script.js
+++ js/script.js
@@ -30,3 +30,28 @@ textarea.addEventListener('input', function() {
     this.style.height = `${this.scrollHeight}px`;
 });
 
+// Get the modal
+var modal = document.getElementById("imageModal");
+
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+var images = document.querySelectorAll(".gallery-item img");
+var modalImg = document.getElementById("modalImage");
+var captionText = document.getElementById("caption");
+
+images.forEach(function(image) {
+    image.onclick = function() {
+        modal.style.display = "block";
+        modalImg.src = this.src;
+        captionText.innerHTML = this.alt;
+    }
+});
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() { 
+    modal.style.display = "none";
+}
+
+
